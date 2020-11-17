b29a0392414b5444e77c303882bb06086507f368
Viachaslau Kalevich <cto@noirix.com>
2020-11-17 21:10:33 +0300

    Spring Boot YAML File reading, datasource autoconfig enable, banner added

--- src/main/java/com/noirix/SpringBootApplicationStarter.java
+++ src/main/java/com/noirix/SpringBootApplicationStarter.java
@@ -1,21 +1,18 @@
 package com.noirix;
 
+import com.noirix.config.AmazonConfig;
 import com.noirix.config.ApplicationBeans;
-import com.noirix.config.DatabaseConfig;
 import com.noirix.config.WebBeansConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication(scanBasePackages = "com.noirix", exclude = {
-        DataSourceAutoConfiguration.class
-})
+@SpringBootApplication(scanBasePackages = "com.noirix")
 @EnableWebMvc
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-@Import({DatabaseConfig.class, ApplicationBeans.class, WebBeansConfig.class})
+@Import({AmazonConfig.class, ApplicationBeans.class, WebBeansConfig.class})
 public class SpringBootApplicationStarter {
 
     public static void main(String[] args) {
